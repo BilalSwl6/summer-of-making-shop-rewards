380cd078d8d36458f5d7df58c677c33039001704
Akash-138-12 <akash456dec@gmail.com>
2025-06-18 04:24:19 +0530

    Added the first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <span>Welcome to the Shop Rewards App. Get to know about all the rewards specific details which we know.</span>
+    </>
   );
 }
 
