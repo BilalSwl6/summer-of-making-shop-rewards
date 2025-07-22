24727110c6529323965a45e73946b27877625c4f
mbilal2913 <mbilal2913@gmail.com>
2025-07-22 22:36:46 +0000

    Add title, Favicon and baseurl

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import GuestLayout from "@/layouts/GuestLayout";
+import { Link } from "react-router-dom";
 
 
 function Home() {
   return (
     <GuestLayout>
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
-      <p className="text-lg">This is the home page.</p>
+      <h1 className="text-4xl font-bold mb-4">I just make only Shop Page</h1>
+      <Link to="/shop" className="text-lg">Click Here!</Link>
     </div>
     </GuestLayout>
   );
